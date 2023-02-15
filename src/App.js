8e9a7024d651fe0622de1ba42258f14384dd6f8a
Nikilraja <nikilrajbca@gmail.com>
2023-02-15 16:18:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MovieSearch from './components/search';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <MovieSearch/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/search/index.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+
+
+function MovieSearch(){
+
+    return(
+        <div>
+            <input type="text" id="movieName" placeholder="enter the movie name"/>
+            <button id="search">search</button>
+        </div>
+    )
+}
+
+export default MovieSearch;
